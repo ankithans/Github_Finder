0ca67180dc440001f1122912a58615665807ba0f
Ankit Hans <ankithans1947@gmail.com>
2020-06-25 17:13:50 +0530

    added basic setup with app.css

---
+++ src/components/users/Users.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import UserItem from './UserItem';
+
+class Users extends Component {
+  state = {
+    users: [
+      {
+        login: 'mojombo',
+        id: 1,
+        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
+        html_url: 'https://github.com/mojombo',
+      },
+      {
+        login: 'defunkt',
+        id: 2,
+        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
+        html_url: 'https://github.com/defunkt',
+      },
+      {
+        login: 'pjhyett',
+        id: 3,
+        avatar_url: 'https://avatars0.githubusercontent.com/u/3?v=4',
+        html_url: 'https://github.com/pjhyett',
+      },
+    ],
+  };
+  render() {
+    return (
+      <div style={userStyle}>
+        {this.state.users.map((user) => (
+          <UserItem key={user.id} user={user}></UserItem>
+        ))}
+      </div>
+    );
+  }
+}
+
+const userStyle = {
+  display: 'grid',
+  gridTemplateColumns: 'repeat(3, 1fr)',
+  gap: '1rem',
+};
+
+export default Users;
