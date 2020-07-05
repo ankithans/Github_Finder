37240b0c20141ad8214e4fa3c80a473aa9e8817c
Ankit Hans <ankithans1947@gmail.com>
2020-07-05 12:52:24 +0530

    added not found page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import User from './components/users/User';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import GithubState from './context/github/GithubState';
 import AlertState from './context/alert/AlertState';
 import './App.css';
@@ -22,8 +23,8 @@ const App = () => {
                 <Route exact path='/' component={Home} />
 
                 <Route exact path='/about' component={About} />
-
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
             </div>
           </div>
---
+++ src/components/pages/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Not Found</h1>
+      <p className='lead'>The page you are looking for doesn't exists.</p>
+    </div>
+  );
+}
